0460ab49cd85fd7829b73aea5bc35fa1b48029a0
NathaliaMiriam <nathalia.miriam@hotmail.com>
2023-02-27 20:12:44 -0300

    starts ViewBinding

--- app/src/main/java/com/example/aplicativo_gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/aplicativo_gastoviagem/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.aplicativo_gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aplicativo_gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
     }
 }
