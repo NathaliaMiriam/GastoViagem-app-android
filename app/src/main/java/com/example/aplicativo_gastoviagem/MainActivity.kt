5257a0c761a66b92f6c605d85a1502ab3b0001a7
NathaliaMiriam <nathalia.miriam@hotmail.com>
2023-02-28 20:31:05 -0300

    ends ViewBinding and starts onclick event

--- app/src/main/java/com/example/aplicativo_gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/aplicativo_gastoviagem/MainActivity.kt
@@ -2,13 +2,27 @@ package com.example.aplicativo_gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.aplicativo_gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding.buttonCalculate.setOnClickListener(this)
     }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+    private fun calculate() {
+
+    }
+    fun teste(view: View) {}
 }
